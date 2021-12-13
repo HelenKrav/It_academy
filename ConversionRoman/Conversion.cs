4bc3894eb590efd0ced7ce374226e0b09a91670e
Alena Kravets <lenoksalnikova@gmail.com>
2021-12-13 14:36:22 +0300

    add code project about roman  conversion in arabian

--- ConversionRoman/Conversion.cs
+++ ConversionRoman/Conversion.cs
@@ -8,23 +8,71 @@ namespace ConversionRoman
 {
     public enum RomanNumericEnum
     {
-        Notnunll,
-        I = 1,
-        V = 5,
-        X =10,
-        L = 50,
-        C = 100,
-        D = 500,
-        M = 1000
+        I,
+        V,
+        X,
+        L,
+        C,
+        D,
+        M
     }
     public class Conversion
     {
-        public RomanNumericEnum numericEnum = RomanNumericEnum.Notnunll;
-        public void Conv(string romanFormat)
+        public int Conv(char[] romanArray)
         {
-
-
-            return;
+            
+            string[] arabArray = new string[romanArray.Length];
+            for (int i = 0; i < romanArray.Length; i++)
+            {
+                if (romanArray[i] == 'M')
+                {
+                    arabArray[i] = "1000";
+                }
+                else if (romanArray[i] == 'D')
+                {
+                    arabArray[i] = "500";
+                }
+                else if (romanArray[i] == 'C')
+                {
+                    arabArray[i] = "100";
+                }
+                else if (romanArray[i] == 'L')
+                {
+                    arabArray[i] = "50";
+                }
+                else if (romanArray[i] == 'X')
+                {
+                    arabArray[i] = "10";
+                }
+                else if (romanArray[i] == 'V')
+                {
+                    arabArray[i] = "5";
+                }
+                else if (romanArray[i]== 'I')
+                {
+                    arabArray[i] = "1";
+                } 
+            }
+            int result = 0;
+            for (int i = 0; i < arabArray.Length; i++)
+            {
+                if (i + 1 < arabArray.Length)
+                {
+                    if (Convert.ToInt32(arabArray[i]) < Convert.ToInt32(arabArray[i + 1]))
+                    {
+                        result -=Convert.ToInt32(arabArray[i]);
+                    }
+                    else
+                    {
+                        result += Convert.ToInt32(arabArray[i]);
+                    }
+                }
+                else
+                {
+                    result += Convert.ToInt32(arabArray[i]);
+                }
+            }
+            return result;
         }
     }
 }
