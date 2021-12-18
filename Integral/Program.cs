79a9700548c6e4b376e76b8c37d47eb7eebd76ba
Alena Kravets <lenoksalnikova@gmail.com>
2021-12-18 21:55:36 +0300

    Add new progects Integral and ParseFormula

---
+++ Integral/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Integral
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите функцию для вычисления с праметром х :");
+            var str = Console.ReadLine();
+
+            try
+            {
+                var x = Expression.Parameter(typeof(double), "x");
+                var expression = ParseFormula.DynamicExpression.ParseLambda(
+                    new ParameterExpression[] { x },
+                    typeof(double),
+                    str);
+
+                var func = (Func<double,double>)expression.Compile();
+
+                var calculate = new СalculateIntegral();
+                calculate.Calculate(func);
+
+
+                var y = func(2);
+
+                Console.Write($"result f(2) = { y }");
+
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+
+
+
+           //Console.ReadLine();
+           // Console.WriteLine("Вернуть значение интеграла");
+           // Console.WriteLine("Вернуть время вычисления");
+            //Console.WriteLine("Вернуть кол-во итераций для каждого значение точности");
+           // Console.WriteLine("________");
+
+           //  integral.Calculate("www");
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ Integral/СalculateIntegral.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Integral
+{
+    
+    
+    /*
+    Рассчать интеграл функции на интервале от -10 до 10 с заданной точностью.Вернуть значение интеграла, время вычисления, кол-во итераций для каждого значение точности.
+- произвольная функция задается в консоли в виде:
+	- 2*x+4
+	- 2*x^2-10
+	- x^3
+- точность 10%, 5%, 1%
+- Тест не нужен
+- Math.Abs(y) - вернет результат по модулю
+- StopWatch()
+     */
+    
+
+    internal class СalculateIntegral
+    {
+        public const int a = -10;
+        public const int b = 10;
+
+        public const string urov1 = "2*x+4";
+        public const string urov2 = "2*x^2-10";
+        public static string urov3 = "x^3";
+        
+        public double Calculate(Func<double, double> func)
+        {
+            for(int i = 10; i < 2;i +=10)
+            {
+              var sInterval = Func(0);
+            }
+            //double result = Convert.ToDouble(new DataTable().Compute("1 + 2 * 7", null));
+          //  Console.WriteLine(result);
+            return 0;
+        }
+
+        public double Func(double x)
+        {
+            return 2*x+10;
+
+        }
+        
+
+
+
+
+    }
+}
